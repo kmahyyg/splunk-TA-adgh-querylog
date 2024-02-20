c0301c27ddc4d2865ef09b7ec7cdbfe4e71e1aa3
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2024-02-21 00:16:18 +0800

    update build and package

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export GOOS=linux
+export GOARCH=amd64
+go build -ldflags='-s -w' -trimpath -o bin/adgh_qlog_preproc.elf ./cmd/main.go
+
+export GOOS=darwin
+export GOARCH=arm64
+go build -ldflags='-s -w' -trimpath -o bin/adgh_qlog_preproc.mach-o ./cmd/main.go
